d540cc3fb6f5d180c80dfde39f38d6f0cd70a091
Oleg <lmorfall@gmail.com>
2019-10-16 10:02:51 +0300

    Доделана работа с атрибутами параметров http запросов

--- HttpServer/HttpServerModule.cs
+++ HttpServer/HttpServerModule.cs
@@ -1,10 +1,14 @@
 ﻿using AppServerBase.Auth;
+using HttpMultipartParser;
 using Newtonsoft.Json.Linq;
 using System;
+using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Reflection;
+using System.Text;
 
 namespace AppServerBase.HttpServer
 {
@@ -75,17 +79,102 @@ namespace AppServerBase.HttpServer
                 }
                 else
                 {
-                    if (method.GetParameters().Count() > 0)
-                        result = method.Invoke(this, new object[] { Body, context });
-                    else
-                        result = method.Invoke(this, new object[] { });
-                    break;
+                    result = method.Invoke(this, ProcessPrameters(method.GetParameters()));
                 }
             }
 
             SetResult(result);
             return result as ServerModuleResponse;
         }
+
+        public static object GetDefault(Type type)
+        {
+            if (type.IsValueType)
+            {
+                return Activator.CreateInstance(type);
+            }
+            return null;
+        }
+
+        private object[] ProcessPrameters(params ParameterInfo[] parameters)
+        {
+            var paramValues = new List<object>();
+            var multipartParams = new Dictionary<string, object>();
+
+            JObject jsonBody = null;
+            if (Context.Request.HttpMethod == "POST"
+                && Context.Response.ContentType.Contains("application/json"))
+            {
+                jsonBody = JObject.Parse(Body);
+            }
+            StreamingMultipartFormDataParser parser = null;
+            if (Context.Response.ContentType.Contains("multipart/form-data"))
+            {
+                
+                parser = new StreamingMultipartFormDataParser(Context.Request.InputStream, Encoding.UTF8);
+
+                parser.ParameterHandler += parameter =>
+                {
+                    multipartParams.Add(parameter.Name, parameter.Data);
+                };
+
+                parser.FileHandler += (name, fileName, type, disposition, buffer, bytes) =>
+                {
+                    multipartParams.Add(name, new { FileName = fileName, Data = bytes });
+                };
+
+                //parser.StreamClosedHandler += () =>
+                //{
+                //    // Do things when my input stream is closed
+                //};
+                parser.Run();
+            }
+            
+            foreach (var param in parameters)
+            {
+                if (param.GetCustomAttributes().Count() == 0)
+                {
+                    paramValues.Add(GetDefault(param.ParameterType));
+                    continue;
+                }
+
+                var valueAttribute = param.GetCustomAttributes().ElementAt(0);
+                var paramNotation = valueAttribute.GetType();
+                var paramName = (valueAttribute as ParamAttribute).ParamName;
+
+                if (paramNotation == typeof(GETParamAttribute)
+                    || paramNotation == typeof(POSTParamAttribute))
+                {
+                    if (UrlParams[paramName] == null)
+                        throw new Exception($"Parameter not given: {paramName}");
+                    paramValues.Add(UrlParams[paramName]);
+                }
+
+                if (paramNotation == typeof(JSONParamAttribute))
+                {
+                    if (!jsonBody.ContainsKey(paramName))
+                        throw new ServerException(ClientMsg.GetErrorMsgInvalidJSON());
+                    paramValues.Add(Convert.ChangeType(jsonBody[paramName], paramNotation));
+                }
+                if ((paramNotation == typeof(JSONObjectParamAttribute))
+                    || (paramNotation == typeof(JSONArrayParamAttribute)))
+                {
+                    if (!jsonBody.ContainsKey(paramName))
+                        throw new ServerException(ClientMsg.GetErrorMsgInvalidJSON());
+                    paramValues.Add(jsonBody[paramName]);
+                }
+                if ((paramNotation == typeof(MultiPartOSPParamAttribute))
+                    || (paramNotation == typeof(MultiPartAPJParamAttribute)))
+                {
+                    if (!multipartParams.ContainsKey(paramName))
+                        throw new Exception($"Parameter not given: {paramName}");
+                    paramValues.Add(multipartParams[paramName]);
+                }
+            }
+
+            return paramValues.ToArray();
+        }
+
 
         protected virtual bool CheckLicense()
         {
